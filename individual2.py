36dd49649a657e93c52f3ff31235a92796d64cf0
Hilol <khilol2017@gmail.com>
2023-12-17 05:24:42 +0300

    update

--- individual2.py
+++
@@ -1,9 +0,0 @@
-def naibolshee_chislo(a, b, c):
-    return max(a, b, c)
-
-num1 = float(input("Введите первое число: "))
-num2 = float(input("Введите второе число: "))
-num3 = float(input("Введите третье число: "))
-
-maximum = naibolshee_chislo(num1, num2, num3)
-print(f"Наибольшее из введенных чисел: {maximum}")
